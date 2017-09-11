e55e4236d00ef79d1219a130a80efcc5e9f4531a
Dan Fornika <dfornika@gmail.com>
2017-09-11 15:27:49 -0700

    Parsing kraken reports

--- multiqc/modules/kraken/kraken.py
+++ multiqc/modules/kraken/kraken.py
@@ -6,7 +6,7 @@ from __future__ import print_function
 
 import logging
 from multiqc import config
-from multiqc.plots import linegraph
+from multiqc.plots import table
 from multiqc.modules.base_module import BaseMultiqcModule
 
 # Initialise the logger
@@ -19,72 +19,50 @@ class MultiqcModule(BaseMultiqcModule):
         href="http://ccb.jhu.edu/software/kraken/",
         info="a system for assigning taxonomic labels to short DNA sequences.")
 
-        self.kraken_data  = dict()
+        self.kraken_report_data  = dict()
         
-        for f in self.find_log_files('kraken', filehandles=True):
-            self.parse_kraken_data(f)
+        for f in self.find_log_files('kraken/report', filehandles=True):
+            self.parse_kraken_report(f)
 
-        if len(self.kraken_data) == 0:
+        if len(self.kraken_report_data) == 0:
             log.debug("Could not find any data in {}".format(config.analysis_dir))
             raise UserWarning
 
-        log.info("Found {} reports".format(len(self.jellyfish_data)))
+        log.info("Found {} reports".format(len(self.kraken_report_data)))
 
         self.plot()
 
 
-    def parse_jellyfish_data(self, f):
-        """ Go through the hist file and memorise it """
-        histogram = {}
-        occurence = 0
+    def parse_kraken_report(self, f):
+        """ Go through the report file and parse it """
+        data = {}
         for line in f['f']:
-            line = line.rstrip('\n')
-            occurence = int(line.split(" ")[0])
-            count = int(line.split(" ")[1])
-            histogram[occurence] = occurence*count
-        #delete last occurnece as it is the sum of all kmer occuring more often than it.
-        del histogram[occurence]
+            line = line.rstrip('\n').split("\t")
+            percentage_reads_clade = float(line[0])
+            number_reads_clade = int(line[1])
+            number_reads_taxon = int(line[2])
+            rank_code = line[3]
+            ncbi_taxonomy_id = line[4]
+            species_name = line[5].lstrip(' ')
+            data[species_name] = percentage
+
         #sanity check
-        self.max_key  = max(histogram, key=histogram.get)
-        self.jellyfish_max_x = max(self.jellyfish_max_x, self.max_key)
-        if len(histogram) > 0:
-            if f['s_name'] in self.jellyfish_data:
-                log.debug("Duplicate sample name found! Overwriting: {}".format(f['s_name']))
-            self.add_data_source(f)
-            self.jellyfish_data[f['s_name']] = histogram
 
 
     def plot(self):
-        """ Generate the qualities plot """
+        """ Generate the plot """
 
         helptext = '''
-            A possible way to assess the complexity of a library even in
-            absence of a reference sequence is to look at the kmer profile of the reads.
-            The idea is to count all the kmers (_i.e._, sequence of length `k`) that occur
-            in the reads. In this way it is possible to know how many kmers occur
-            `1,2,.., N` times and represent this as a plot.
-            This plot tell us for each x, how many k-mers (y-axis) are present in the
-            dataset in exactly x-copies.
-            
-            In an ideal world (no errors in sequencing, no bias, no  repeated regions)
-            this plot should be as close as  possible to a gaussian distribution.
-            In reality we will always see a peak for `x=1` (_i.e._, the errors)
-            and another peak close to the expected coverage. If the genome is highly
-            heterozygous a second peak at half of the coverage can be expected.'''
+        '''
 
         pconfig = {
             'id': 'kraken',
-            'title': 'Kraken: K-mer plot',
-            'ylab': 'Counts',
-            'xlab': 'k-mer frequency',
-            'xDecimals': False,
-            'xmin': xmin,
-            'xmax': xmax
+            'title': 'Kraken: Report Table',
         }
 
         self.add_section(
             anchor = 'kraken',
-            description = 'The K-mer plot lets you estimate library complexity and coverage from k-mer content.',
+            description = '',
             helptext = helptext,
-            plot = linegraph.plot(self.jellyfish_data, pconfig)
+            plot = table.plot(self.kraken_report_data, pconfig)
         )
