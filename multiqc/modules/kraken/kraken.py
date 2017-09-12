46c8e3193445ff0a8cd696d1d23bed5e78ca229c
Dan Fornika <dfornika@gmail.com>
2017-09-12 14:15:41 -0700

    Only plot percent unclassified

--- multiqc/modules/kraken/kraken.py
+++ multiqc/modules/kraken/kraken.py
@@ -3,8 +3,9 @@
 """ MultiQC module to parse results from kraken  """
 
 from __future__ import print_function
-
+from collections import OrderedDict
 import logging
+
 from multiqc import config
 from multiqc.plots import table
 from multiqc.modules.base_module import BaseMultiqcModule
@@ -19,23 +20,29 @@ class MultiqcModule(BaseMultiqcModule):
         href="http://ccb.jhu.edu/software/kraken/",
         info="a system for assigning taxonomic labels to short DNA sequences.")
 
-        self.kraken_report_data  = {}
-        
-        for f in self.find_log_files('kraken/report', filehandles=True):
-            self.kraken_report_data[f['s_name']] = self.parse_kraken_report(f)
+        self.kraken  = {}
 
-        if len(self.kraken_report_data) == 0:
+        for f in self.find_log_files('kraken/report', filehandles=True):
+            self.parse_kraken(f)
+            
+        if len(self.kraken) == 0:
             log.debug("Could not find any data in {}".format(config.analysis_dir))
             raise UserWarning
 
-        log.info("Found {} reports".format(len(self.kraken_report_data)))
+        log.info("Found {} reports".format(len(self.kraken)))
 
-        self.plot()
+        self.add_section( plot = self.kraken_table() )
 
 
-    def parse_kraken_report(self, f):
+    def parse_kraken(self, f):
         """ Go through the report file and parse it """
-        data = {}
+        s_name = f['s_name']
+        if getattr(config, 'kraken_fn_snames', False):
+            s_name = f['s_name']
+        if f['s_name'] in self.kraken:
+            log.debug("Duplicate sample name found! Overwriting: {}".format(f['s_name']))
+
+        self.kraken[s_name] = {}
         for line in f['f']:
             line = line.rstrip('\n').split("\t")
             percentage_reads_clade = float(line[0])
@@ -43,29 +50,17 @@ class MultiqcModule(BaseMultiqcModule):
             number_reads_taxon = int(line[2])
             rank_code = line[3]
             ncbi_taxonomy_id = line[4]
-            species_name = line[5].lstrip(' ')
-            data[species_name] = {}
-            data[species_name]['NCBI Taxonomy ID'] = ncbi_taxonomy_id
-            data[species_name]['Percent Reads in Clade'] = percentage_reads_clade
+            clade_name = line[5].lstrip(' ')
+            if clade_name == "unclassified":
+                self.kraken[s_name]['Percent Unclassified'] = percentage_reads_clade
+        self.add_data_source(f, s_name)
 
-        # sanity check
-        # TODO
-        return data
+    def kraken_table(self):
+        """ Generate the table """
+        headers = OrderedDict()
 
-    def plot(self):
-        """ Generate the plot """
-
-        helptext = '''
-        '''
-
-        pconfig = {
-            'id': 'kraken',
-            'title': 'Kraken: Report Table',
+        table_config = {
+            'namespace': 'kraken'
         }
 
-        self.add_section(
-            anchor = 'kraken',
-            description = '',
-            helptext = helptext,
-            plot = table.plot(self.kraken_report_data, pconfig)
-        )
+        return table.plot(self.kraken, headers, table_config)
